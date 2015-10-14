5fef1e18cff4c26353b1760519b59454e8bd5ea2
Ryan Wechter <ryan.wechter@utexas.edu>
2015-10-13 20:03:20 -0500

    Data uploaded to SQL)

---
+++ 00 Doc/R_ETL.R
@@ -0,0 +1,37 @@
+require(tidyr)
+require(dplyr)
+require(ggplot2)
+
+setwd("/Users/Ryan_Wechter/DataVisualization/DV_RProject3/01\ Data/provider-state-estimates/")
+
+file_path <- "US_AGGREGATE09.csv"
+
+df <- read.csv(file_path, stringsAsFactors = FALSE)
+
+# str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+
+dimensions <- c("Code","Item","Group","Region_Number","Region_Name","State_Name")
+
+
+
+measures <- setdiff(names(df), dimensions)
+
+
+sql <- paste("CREATE TABLE", tableName, "(\n-- Change table_name to the table name you want.\n")
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    sql <- paste(sql, paste(d, "varchar2(4000),\n"))
+  }
+}
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    if(m != tail(measures, n=1)) sql <- paste(sql, paste(m, "number(38,4),\n"))
+    else sql <- paste(sql, paste(m, "number(38,4)\n"))
+  }
+}
+sql <- paste(sql, ");")
+cat(sql)
+ 
+ 
+
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from NHCE"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_riw223', PASS='orcl_riw223', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
