98286881195f93943e5715fc60f1d1c4eb52fe42
Ryan Wechter <ryan.wechter@utexas.edu>
2015-10-13 20:15:49 -0500

    Starting Data Wrangling

--- 00 Doc/R_ETL.R
+++ 00 Doc/R_ETL.R
@@ -38,4 +38,4 @@ NHCE <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?q
 
 COD <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from COD"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_riw223', PASS='orcl_riw223', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-
+NHCE %>% filter(COUNTRYGROUP != "Region") %>% 
