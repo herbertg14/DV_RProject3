1b7fadf3fea3c26eb960bb7241a9b10dfbc2c060
herbertg14 <herbert.gutierrez@utexas.edu>
2015-10-16 10:51:10 -0500

    Made Graph two and started RMD

--- 02 Data Wrangling/Graph3.R
+++ 02 Data Wrangling/Graph3.R
@@ -1,18 +1,19 @@
-df1 <- COD %>% select(STATE,YEAR,AADR,CAUSE_NAME) %>% filter(YEAR == 2009 & CAUSE_NAME == "Suicide")
+df1 <- COD %>% select(STATE,YEAR,AADR,CAUSE_NAME,DEATHS) %>% filter(YEAR == 2009 & CAUSE_NAME == "Suicide")
 
 df2 <- NHCE %>% filter(CODE == 7,COUNTRYGROUP == "State") %>% select(ITEM,STATE_NAME,Y2009) %>% rename(STATE = STATE_NAME, SPENDING = Y2009)
 
 df <- dplyr::inner_join(df1,df2, by = "STATE")
 
-
 ggplot() +
   coord_cartesian() +
   scale_x_continuous() + 
   scale_y_continuous() + 
-  labs(title = "State spending vs. AADR") +
-  labs(x= "Spending",y = "AADR") +
+  labs(title = "Prescription Drug Spending vs. Number of Deaths") +
+  labs(x= "Spending (Millions of Dollars)",y = "Number of Deaths") +
+  theme_gray() +
+  theme(plot.title=element_text(size=20, face="bold", vjust=2)) +
   layer(data = df,
-        mapping = aes(x = SPENDING, y = AADR),
+        mapping = aes(x = SPENDING, y = DEATHS),
         stat="identity", 
         stat_params=list(),
         geom="point",geom_params=list(),
---
+++ 02 Data Wrangling/Project3_ETL.R
@@ -0,0 +1,21 @@
+require("ggplot2")
+require("gplots")
+require("grid")
+require("RCurl")
+require("reshape2")
+require("tableplot")
+require("tidyr")
+require("dplyr")
+require("jsonlite")
+require("lubridate")
+
+
+NHCE <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from NHCE"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_riw223', PASS='orcl_riw223', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+summary(NHCE)
+head(NHCE)
+
+COD <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from COD"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_riw223', PASS='orcl_riw223', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+summary (COD)
+head(COD)
