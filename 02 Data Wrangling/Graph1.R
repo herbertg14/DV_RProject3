77dd7a96c2f28851f91a13bb3a71eaa9294b4cea
Ryan Wechter <ryan.wechter@utexas.edu>
2015-10-16 17:38:38 -0500

    Finished graph

--- 02 Data Wrangling/Graph1.R
+++ 02 Data Wrangling/Graph1.R
@@ -4,7 +4,7 @@ df2 <- NHCE %>% select(ITEM,STATE_NAME,Y1999,CODE,COUNTRYGROUP) %>% filter(CODE 
 
 df <- dplyr::full_join(df1,df2,by = "STATE")
 
-df %>% ggplot(aes(x=reorder(STATE,SPENDING/DEATHS),y=SPENDING/DEATHS ,fill = STATE)) + geom_bar(stat = 'identity') + theme(legend.position = "none") + labs(title = "State Spending on Hospitals per Cancer Death",x= "STATE",y = "Dollars per Deaths") + theme(axis.text.x = element_text(angle = 45,size = 10))
+df %>% ggplot(aes(x=reorder(STATE,SPENDING/DEATHS),y=SPENDING/DEATHS ,fill = STATE)) + geom_bar(stat = 'identity') + theme(legend.position = "none") + labs(title = "Total Spending on Hospitals per Cancer Death",x= "STATE",y = "Dollars per Deaths") + theme(axis.text.x = element_text(angle = 45,size = 10))
 
 
 
